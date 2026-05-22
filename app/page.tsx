44809d548f40855848c8c6b899a75d5805db5c11
rbmininibus <loris.rosain@gmail.com>
2026-05-22 12:44:43 +0200

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -1,8 +1,8 @@
 export default function Home() {
   return (
-    <main style={{padding: 40}}>
+    <main style={{padding:40}}>
       <h1>Réservation Minibus RB</h1>
-      <p>Site en ligne ✅</p>
+      <p>Le site fonctionne ✅</p>
     </main>
   )
 }
