013062d60dced09a55c7e3abb9b18b81b967fb64
rbmininibus <loris.rosain@gmail.com>
2026-05-22 14:55:48 +0200

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,3 +1,11 @@
+import type { Metadata } from 'next'
+import './globals.css'
+
+export const metadata: Metadata = {
+  title: 'Racing Besançon Minibus',
+  description: 'Réservation de minibus pour le Racing Besançon',
+}
+
 export default function RootLayout({
   children,
 }: {
