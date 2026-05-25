8e1e9c87c96b8c2666a7ba9c376541f4cc10d1a6
rbmininibus <loris.rosain@gmail.com>
2026-05-25 19:13:59 +0200

    Update supabase.ts

--- lib/supabase.ts
+++ lib/supabase.ts
@@ -1,8 +1,9 @@
 import { createClient } from '@supabase/supabase-js'
 
 const supabaseUrl = process.env.NEXT_PUBLIC_SUPABASE_URL!
-const supabaseKey = typeof window === 'undefined'
-  ? process.env.SUPABASE_SERVICE_ROLE_KEY!
-  : process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!
+const supabaseAnonKey = process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!
+const supabaseServiceKey = process.env.SUPABASE_SERVICE_ROLE_KEY!
 
-export const supabase = createClient(supabaseUrl, supabaseKey)
+export const supabase = createClient(supabaseUrl, supabaseAnonKey)
+
+export const supabaseAdmin = createClient(supabaseUrl, supabaseServiceKey)
